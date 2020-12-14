35f74f11efc9d3ea86a8688d92e7c43aad480d9b
Guy de Messimy <gdemessimy@gmail.com>
2020-12-14 11:54:31 +0100

    refactorisation ok

--- capitalizeFirst.js
+++ capitalizeFirst.js
@@ -1,10 +1,10 @@
 const assert = require('assert');
 
-capitalizeFirst(input) {
+function capitalizeFirst(input) {
     return input.length > 0
     ? input[0].toUpperCase() + input.slice(1)
     : "";
-}
+};
 
 assert.strictEqual(typeof capitalizeFirst, 'function');
 assert.strictEqual(capitalizeFirst.length, 1);
